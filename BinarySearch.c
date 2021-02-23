93e8190c67b387258b6ba800b941acd47adc1aa5
RektByDead <rlousada123456@gmail.com>
2021-02-23 15:11:46 +0000

    Code changes

--- BinarySearch.c
+++ BinarySearch.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define TAM 1000
+#define TAM 99999
 
 int main(){
 
@@ -30,20 +30,20 @@ int main(){
     tries = 1;
 
     //smart way to do it (I would never do something like this because I'm lazy
-    while(tries++ != -1) {
-        static int middlePos = TAM/2;
+    int middlePos = TAM/2;
+    while(array[middlePos - 1] != randomNumber) {
         static int anterior = 0;
         int divisor = round((float)abs(middlePos - anterior)/2.0);
 
         anterior = middlePos;
-
-        if (array[middlePos - 1] == randomNumber)
-            break;
-        else if (randomNumber > array[middlePos - 1]) //always -1 because "middlePos" could have TAM as value
+        if (randomNumber > array[middlePos - 1]) //always -1 because "middlePos" could have TAM as value
             middlePos += divisor; 
-        else if (randomNumber < array[middlePos - 1])//always -1 because "middlePos" could have TAM as value
+        else //if (randomNumber < array[middlePos - 1]) - always -1 because "middlePos" could have TAM as value
             middlePos -= divisor; 
+        
+        tries++;
     }
-    printf("\nSmart way to do it -> %d iterations", tries);
 
+    printf("\nSmart way to do it -> %d iterations", tries);
+    printf("\nThe number %d is on index %d", array[middlePos - 1], middlePos - 1);
 }
